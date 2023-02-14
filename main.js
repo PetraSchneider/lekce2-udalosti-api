7035e0cb608cdb37fe3fb3b1e03ce4a5b9ba5b2f
Petra Schneiderova <pe.schneiderova@gmail.com>
2023-02-14 20:14:21 +0100

    posun

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // tady je místo pro náš program
+function zmenaPozadi(){
+    let text;
+    text = document.querySelector('.odstavec');
+    text.classList.toggle('.tridaOdsatvce');
+}
+
+   let pismo;
+   pismo = document.querySelector('p')
+   
