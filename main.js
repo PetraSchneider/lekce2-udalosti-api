f49943993e8e5dc37f75d24fd1e06c0b200be304
Petra Schneiderova <pe.schneiderova@gmail.com>
2023-02-15 14:33:38 +0100

    posun

--- main.js
+++ main.js
@@ -2,9 +2,35 @@
 function zmenaPozadi(){
     let text;
     text = document.querySelector('.odstavec');
-    text.classList.toggle('.tridaOdsatvce');
+    text.classList.toggle('tridaOdsatvce');
+}
+function zmenaVelikosti(event){
+   let pismo;
+   pismo = document.querySelector('.odstavec')
+   pismo.style.fontSize = '+ 1px'
 }
 
-   let pismo;
-   pismo = document.querySelector('p')
-   
+let audio = document.querySelector('.zvuk')
+
+function spustaudio(){
+    audio.play();
+}
+function zastavtaudio(){
+    audio.pause();
+}
+function hlasitost0(){
+    audio.volume = 0;
+}
+function hlasitost05(){
+    audio.volume = 0.5;
+}
+function hlasitost1(){
+    audio.volume = 1;
+}
+function resettaudio(){
+    audio.currentTime = 0;
+}
+zvuk.play();
+zvuk.pause();
+zvuk.currentTime = 0;
+zvuk.volume = 0.5;
